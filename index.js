dcad8748349aa77ef9d42736063bfa9a00d0e0fa
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-09 12:32:57 -0500

    added home page view

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //import router from places.js
 app.use('/places',require('./controllers/places'))
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 //wildcard route with status response
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
