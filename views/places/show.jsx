50f0ff728c46a4f5e7131897484aee988cc232bb
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-12 11:12:30 -0500

    show route connected

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,15 @@ function show () {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+              Edit
+            </a>     
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form> 
+
             <div>
                 <h2>Rating</h2>
                 <p>currently unrated</p>
