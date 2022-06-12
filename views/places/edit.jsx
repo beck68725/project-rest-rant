f1dc733e7d1f5467fc9d631f26c09e55f7f06862
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-12 13:03:39 -0500

    added edit route

---
+++ views/places/edit.jsx
@@ -0,0 +1,39 @@
+const React = require('react')
+const Def = require('../default.jsx')
+
+function edit_form (data) {
+    return (
+        <Def>
+          <main>
+            <h1>Edit Place</h1>
+            <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <div className='row'>
+                    <div className='form-group col-sm-6'>
+                        <label htmlFor="name">Place Name</label>
+                        <input id="name" name="name" value={data.place.name}required />
+                    </div>
+                    <div className='form-group col-sm-6'>
+                        <label htmlFor="pic">Place Picture</label>
+                        <input id="pic" name="pic" value={data.place.pic}required />
+                    </div>
+                    <div className='form-group col-sm-6'>
+                        <label htmlFor="city">City</label>
+                        <input id="city" name="city" value={data.place.city}required />
+                    </div>
+                    <div className='form-group col-sm-6'>
+                        <label htmlFor="state">State</label>
+                        <input id="state" name="state" value={data.place.state}required />
+                    </div>
+                    <div className='form-group col-sm-6'>
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input id="cuisines" name="cuisines" value={data.place.cuisines}required />
+                    </div>
+                    <input className='btn btn-primary' type="submit" value="Add Place" />
+                </div>
+            </form>
+          </main>
+        </Def>
+    )
+}
+
+module.exports = edit_form
