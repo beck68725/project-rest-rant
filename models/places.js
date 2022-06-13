6af1286d007a66a233a379ef288c8ea73a6d6ade
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-12 20:47:17 -0500

    fix bugs

--- models/places.js
+++ models/places.js
@@ -1,13 +1,14 @@
 module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
-}]
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/Users/rebeccacoburn/Desktop/backEnd/rest-rant/public/image/eaters-collective-ddZYOtZUnBk-unsplash.jpg'
+      }, {
+          name: 'Coding Cat Cafe',
+          city: 'Phoenix',
+          state: 'AZ',
+          cuisines: 'Coffee, Bakery',
+          pic: '/Users/rebeccacoburn/Desktop/backEnd/rest-rant/public/image/demi-deherrera-L-sm1B4L1Ns-unsplash.jpg'
+      }]
+      
--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home () {
             <main>
                 <h1>REST-Rant</h1>
                 <div>
-                    <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                    <img src="/images/chia-fruit-drink.jpg/400/400" alt="Chia Fruit Shake" />
                 <div>
                     Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
                 </div>
