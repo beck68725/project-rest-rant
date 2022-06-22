b5b9895b17a3bc77a83afc5bb6e52dde5708e47b
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-21 21:19:06 -0500

    added values to places

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,7 +5,25 @@ function show (data) {
     return (
         <Def>
           <main>
+          <div className="row">
+            <div className="col-sm-6">
+              <img src={data.place.pic} alt={data.place.name} />
+              <h3>
+                Located in {data.place.city}, {data.place.state}
+              </h3>
+            </div>
+            <div className="col-sm-6">
             <h1>{ data.place.name }</h1>
+            <h2>
+              Description
+            </h2>
+            <h3>
+              {data.place.showEstablished()}
+            </h3>
+            <h4>
+              Serving {data.place.cuisines}
+            </h4>
+            </div>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>     
@@ -23,6 +41,7 @@ function show (data) {
                 <h2>Comments</h2>
                 <p>No comments yet!</p>
             </div>
+          </div>
           </main>
         </Def>
     )
