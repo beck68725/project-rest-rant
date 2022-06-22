d7af9a21c9023cab7526a4a25c14e21542ae8a42
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-06-21 20:23:12 -0500

    added seeders folder and seed places file

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+require('dotenv').require()
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -6,3 +7,4 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 
 module.exports.Place = require('./places')
+
